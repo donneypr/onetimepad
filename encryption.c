4df12864b1e2a82f14cb35b77e23473ac6ab967e
donneypr <donatoprabahar@gmail.com>
2024-12-02 15:37:36 -0500

    open secret file

--- encryption.c
+++ encryption.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 1) {
+        printf("Add the file to be encrypted");
+    } else {
+        char* secret_file_name = argv[1];
+        FILE* secret_file_name = fopen(secret_file_name,"r");
+    }
+}
